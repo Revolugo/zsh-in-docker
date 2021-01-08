f6e64a7e5f21457ed5b16090b688d4479a950cc9
David <david@Davids-MacBook-Pro.local>
2021-01-08 23:30:29 +0100

    Fixed script

--- zsh-in-docker.sh
+++ zsh-in-docker.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
 
-wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.1/zsh-in-docker.sh -- \
+sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.1/zsh-in-docker.sh)" -- \
     -t https://github.com/bhilburn/powerlevel9k\
+    -p git-flow \
+    -p git-prompt \
+    -p node \
+    -p npm \
+    -p npx \
+    -p zsh_reload \
     -p https://github.com/bobthecow/git-flow-completion \
     -p https://github.com/zsh-users/zsh-autosuggestions \
     -p https://github.com/zsh-users/zsh-syntax-highlighting \
@@ -10,8 +16,10 @@ wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.1/zsh-in
     -a 'export TERM="xterm-256color"' \
     -a 'POWERLEVEL9K_PROMPT_ON_NEWLINE=true' \
     -a 'POWERLEVEL9K_PROMPT_ADD_NEWLINE=true' \
-    -a 'POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)' \
+    -a 'POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(user dir vcs status)' \
     -a 'POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)' \
-    -a 'POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%f"'
+    -a 'POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=""' \
+    -a 'POWERLEVEL9K_SHORTEN_STRATEGY="truncate_to_last"' \
+    -a 'POWERLEVEL9K_STATUS_CROSS=true'
 
 apt-get install git-flow
